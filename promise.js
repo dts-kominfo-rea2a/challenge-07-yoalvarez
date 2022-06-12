f962501b4560ba78d562fe0886b7f70a4df0ce0d
yoalvarez <85821970+yoalvarez@users.noreply.github.com>
2022-06-12 20:47:13 +0700

    Commit Pertama

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+    
+  let theaterIXX = await promiseTheaterIXX();
+  let theaterVGC = await promiseTheaterVGC();
+
+  // Data penonton theater IXX dijumlahkan dgn data theater VGC menggunakan array concat
+  let theaterData = theaterIXX.concat(theaterVGC);
+  // Mengembalikan nilai length dari array yang telah difilter berdasarkan hasil emosi
+  return theaterData.filter(element => (element.hasil === emosi)).length;
+};
 
 module.exports = {
   promiseOutput,
